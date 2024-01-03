60c94cd5bcd12bb2d6ba036d4e0dcca77c3642b0
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-03 17:49:15 -0600

    following the lessons

--- notes.cpp
+++ notes.cpp
@@ -1,5 +1,16 @@
 // Booleans
-void f(int a, int b)
+/* void f(int a, int b)
 {
   bool b1 = a == b;
+} */
+
+// by definition true has the value of 1 and false has the value of 0
+
+#include <iostream>
+
+int main()
+{
+  char c;
+  std::cin >> c;
+  std::cout << "the value of'"
 }
