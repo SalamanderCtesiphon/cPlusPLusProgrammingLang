645a01a87f52dd0559885708ca79e4b344638c92
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-04 13:40:54 -0600

    copying lesson

--- chapter1/lesson1.3.cpp
+++ chapter1/lesson1.3.cpp
@@ -13,10 +13,21 @@
 
   return 0;
 } */
-
+/*
 int main()
 {
   int x{5};
+
+  return 0;
+} */
+
+int main()
+{
+  [[maybe_unused]] double pi{3.14159};
+  [[maybe_unused]] double gravity{9.8};
+  [[maybe_unused]] double phi{1.61803};
+
+  // the above variables will not generate unused variable warnings
 
   return 0;
 }
