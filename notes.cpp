4a799291fb9c0e1dcf929afc61cd20fa5d096f03
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-03 21:24:20 -0600

    got rid of an  intellesense warning line

--- notes.cpp
+++ notes.cpp
@@ -14,13 +14,13 @@ int main()
   return 0;
 } */
 
-/* #include <iostream>
+#include <iostream>
 #include <vector>
 #include <string>
 
-using namespace std; */
+using namespace std;
 
-/* int main()
+int main()
 {
   vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
@@ -29,39 +29,4 @@ using namespace std; */
     cout << word << " ";
   }
   cout << endl;
-} */
-/* #include <array>
-#include <iostream>
-#include <span>
-
-struct Foo
-{
-  int a{};
-  int b{};
-  int c{};
-};
-
-consteval int sum(std::span<const int> a) // std::span and consteval
-{
-  int s{0};
-  for (auto n : a)
-    s += n;
-  return s;
 }
-
-auto sum(auto x, auto y) -> decltype(x + y) // abbreviated function templates
-{
-  return x + y;
-}
-
-int main()
-{
-  constexpr std::array a{3, 2, 1};
-  constexpr int s{sum(a)};
-  std::cout << s << '\n';
-
-  Foo f1{.a = 1, .c = 3}; // designated initializers
-  std::cout << sum(f1.a, f1.c) << '\n';
-
-  return 0;
-} */
