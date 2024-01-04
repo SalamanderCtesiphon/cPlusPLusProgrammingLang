6a8b0f164fe1f8b5e56ff08409015eb89cd4cf32
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-03 21:08:10 -0600

    installed cpp essentials for vscode and tested to see if it was working

--- notes.cpp
+++ notes.cpp
@@ -6,10 +6,27 @@
 
 // by definition true has the value of 1 and false has the value of 0
 
-#include <iostream>
+/* #include <iostream>
 
 int main()
 {
   std::cout << "Here is some text.";
   return 0;
+} */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+  for (const string &word : msg)
+  {
+    cout << word << " ";
+  }
+  cout << endl;
 }
