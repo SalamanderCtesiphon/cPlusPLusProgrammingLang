edbe761a5efc706e17b37a1afb2b4d2d160873bd
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-04 19:39:30 -0600

    attempted to check out the effects of printing out an uninitialized variable but the results was just 0. really not very exciting tbh

--- chapter1/lesson1.3.cpp
+++ chapter1/lesson1.3.cpp
@@ -1,33 +1,10 @@
 #include <iostream>
 
-/* int main()
-{
-  int width;
-  width = 5;
-
-  std::cout << width;
-
-  width = 7;
-
-  std::cout << width;
-
-  return 0;
-} */
-/*
 int main()
 {
-  int x{5};
+  int x;
 
-  return 0;
-} */
-
-int main()
-{
-  [[maybe_unused]] double pi{3.14159};
-  [[maybe_unused]] double gravity{9.8};
-  [[maybe_unused]] double phi{1.61803};
-
-  // the above variables will not generate unused variable warnings
+  std::cout << x << '\n';
 
   return 0;
 }
