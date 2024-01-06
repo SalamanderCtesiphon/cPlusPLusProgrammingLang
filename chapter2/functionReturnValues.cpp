51feb67a15ed0cbfbde525c386a435b99a9ae92e
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-06 11:38:24 -0600

    rewrote the previous program that makes a function call taht asks a user to enter an integer. The program then out puts the number multiplied by two

--- chapter2/functionReturnValues.cpp
+++ chapter2/functionReturnValues.cpp
@@ -12,8 +12,7 @@ int getValueFromUser()
 int main()
 {
   int num{getValueFromUser()};
-
-  std::cout << num << " doubled is: " << num * 2 << '\n';
+  std::cout << num << "double is: " << num * 2 << '\n';
 
   return 0;
 }
